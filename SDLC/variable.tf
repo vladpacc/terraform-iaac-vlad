f0dea0935c280230c6a987342dd73c8946d41b95
EC2 Default User <ec2-user@ip-172-31-25-115.eu-west-2.compute.internal>
2020-05-26 21:10:17 +0000

    up

---
+++ SDLC/variable.tf
@@ -0,0 +1,21 @@
+variable "environment" {} 
+
+variable "s3_bucket" {} 
+
+variable "s3_folder_project" {} 
+
+variable "s3_folder_region" {} 
+
+variable "s3_folder_type" {} 
+
+variable "s3_tfstate_file" {} 
+
+variable "region" {}
+
+variable "public_cidr1" {}
+variable "public_cidr2" {}
+variable "public_cidr3" {}
+
+variable "private_cidr1" {}
+variable "private_cidr2" {}
+variable "private_cidr3" {}
---
+++ SDLC/vpc.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  source  = "farrukh90/vpc/aws"
+  region = "${var.region}"
+  vpc_cidr = "10.0.0.0/16"
+  public_cidr1 = "${var.public_cidr1}"
+  public_cidr2 = "${var.public_cidr2}"
+  public_cidr3 = "${var.public_cidr3}"
+  private_cidr1 = "${var.private_cidr1}"
+  private_cidr2 = "${var.private_cidr2}"
+  private_cidr3 = "${var.private_cidr3}"
+  tags    =   {
+    Name                    =   "VPC_Project"
+    Environment             =   "Dev"
+    Team                    =   "DevOps"
+    Created_by              =   "Farrukh"
+    Department              =   "IT"
+    Bill                    =   "CFO"
+   }
+}
